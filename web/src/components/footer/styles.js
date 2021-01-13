8f1d645a3df6de68a1d2a3f6f6d19974a556ee4c
GabrielFelix23 <srgfelix123@gmail.com>
2021-01-12 22:38:20 -0300

    Footer pronto

---
+++ web/src/components/footer/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 30px;
+    position: fixed;
+    bottom: 0;
+    background: #F53131;
+    border-top: 3px solid #CC0000;
+
+    small{
+        font-size: 16px;
+        color: #fff
+    }
+`
--- web/src/views/home/index.js
+++ web/src/views/home/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import * as S from './styles'
 
 import Header from '../../components/header'
+import Footer from '../../components/footer'
 
 function Home() {
   return (
     <S.Container>
       <Header/>
       <h1>Home</h1>
+      <Footer/>
     </S.Container>
   );
 }
